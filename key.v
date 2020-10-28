ec7e410df3e5e6be99d7030c094923ee1975669a
lixinliangwork@gmail.com <lixinliangwork@gmail.com>
2020-10-28 22:13:15 +0800

    待添加测试文件

--- key.v
+++ key.v
@@ -1,24 +1,17 @@
 module key(
     input       sys_clk,
     input       sys_rst,
-    input       key,
-    output reg  key_status   /* 按键状态，0：默认；1：上升沿；2：下降沿 */
+    input       key_in,
+    output reg  key_status   /* 按键状态 */
 );
 
-localparam KEY_IDLE     = 2'd0;
-localparam KEY_POSEDGE  = 2'd1;
-localparam KEY_NEGEDGE  = 2'd2;
+wire       key_edge;    /* 按键跳变 */
+reg [19:0] key_cnt;     /* 按键状态保持时间 */
+reg        key_k0;      /* 当前按键值 */
+reg        key_k1;      /* 前一秒按键值 */
+reg        key_next;    /* 按键下一时刻状态 */
 
-reg key_posedge; /* 按键跳变 */
-reg key_negedge; 
-reg key_edge_flag;  /* 跳变沿状态标记，0：空闲；1：上升沿计数过程；2：下降沿计数过程 */
-reg key_cnt;        /* 按键状态保持时间 */
-reg key_k0;     /* 当前按键值 */
-reg key_k1;     /* 前一秒按键值 */
-reg key_next;   /* 按键下一时刻状态 */
-
-assign key_posedge = key_k0 & (~key_k1);    /* 上升沿 */
-assign key_negedge = (~key_k0) & key_k1;    /* 下降沿 */
+assign key_edge = (key_k0 == key_k1)? 1'b0:1'b1;
 
 always @(posedge sys_clk or negedge sys_rst) begin
     if (!sys_rst) begin 
@@ -26,7 +19,7 @@ always @(posedge sys_clk or negedge sys_rst) begin
         key_k1 <= 0;
     end
     else begin
-        key_k0 <= key;
+        key_k0 <= key_in;
         key_k1 <= key_k0;
     end
 end
@@ -34,8 +27,7 @@ end
 /* 状态改变 */
 always @(posedge sys_clk or negedge sys_rst) begin
     if (!sys_rst) begin 
-        key_status  <= KEY_IDLE;
-        key_next    <= KEY_IDLE;
+        key_status  <= 0;
     end
     else begin
         key_status <= key_next;
@@ -43,35 +35,31 @@ always @(posedge sys_clk or negedge sys_rst) begin
 end
 
 /* 下一时刻按键状态修改 */
-always @(*) begin
-    case (key_status)
-        KEY_IDLE: begin 
-            if () begin
-            
-            end
-            else begin
-            
-            end
-        end
-        KEY_POSEDGE: begin
-        
-        end
-        KEY_NEGEDGE: begin
-        
-        end
-        default: begin
-        
-        end
-    endcase
-end
-
 always @(posedge sys_clk or negedge sys_rst) begin
     if (!sys_rst) begin
-        key_edge_flag <= 2'd0;
-        key_cnt <= 20'd0;
+        key_next <= 0;
+    end
+    else if (key_cnt == 20'd1000_000) begin
+        key_next <= key_in;
     end
     else begin
-    
+        key_next <= key_next;
+    end
+end
+
+/* 计数 */
+always @(posedge sys_clk or negedge sys_rst) begin
+    if (!sys_rst) begin  /* 复位 */
+        key_cnt <= 20'd0;
+    end
+    else if (key_edge == 1) begin /* 跳变沿 */
+        key_cnt <= 1'b0;
+    end
+    else if ((key_edge == 0) && (key_cnt < 20'd1000_000)) begin
+        key_cnt <= key_cnt + 1'b1;
+    end
+    else begin
+        key_cnt <= 20'd1000_000;
     end
 end
 endmodule 
